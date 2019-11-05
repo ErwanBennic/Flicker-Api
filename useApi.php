2e94b682e04ee7011549aa75a11494aacc4a641b
Erwan Bennic <erwanb04@gmail.com>
2019-11-05 22:27:48 +0100

    1.0

--- useApi.php
+++ useApi.php
@@ -1,5 +1,14 @@
 <?php
-require 'vendor/autoload.php';
+ini_set("allow_url_fopen", 1);
 
-$m = new MongoClient(); // connexion
-$db = $m->selectDB("Flicker-Api");
+if ( isset( $_POST['keyword'])) {
+    $keyword = $_POST['keyword'];
+    echo $keyword;
+    echo "<br>";
+    $json = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=08a1a1fb2786ca00e3801c9048e262e9&tags='.$keyword.'&safe_search=3&format=json&nojsoncallback=1';
+    $contents = file_get_contents($json);
+    $contents = utf8_encode($contents);
+    $obj = json_decode($contents);
+    var_dump($obj);
+}
+?>
