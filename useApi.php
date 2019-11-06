e822b34d856bcd9155e4beb94eb34bf4f55c137a
Erwan Bennic <erwanb04@gmail.com>
2019-11-06 21:19:11 +0100

    PHP
    Vérification si aucun résultat trouvé

--- useApi.php
+++ useApi.php
@@ -10,19 +10,23 @@ $key = 1;
 
 if ( isset( $_POST['keyword'])) {
     $keyword = $_POST['keyword'];
-    echo $keyword;
+    echo "Recherche pour \"$keyword\".";
     echo "<br>";
     $json = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=974d84d5c54c0d30e46e31aee0df863f&tags='.$keyword.'&format=json&nojsoncallback=1';
     $contents = file_get_contents($json);
     $contents = utf8_encode($contents);
     $obj = json_decode($contents);
+    if (!empty($obj->photos->photo[$key]) && is_object($obj->photos->photo[$key])) {
+        while ($key < 50) {
 
-    while ($key < 50) {
-        $results[$key]["farm"] = $obj->photos->photo[$key]->farm;
-        $results[$key]["server"] = $obj->photos->photo[$key]->server;
-        $results[$key]["id"] = $obj->photos->photo[$key]->id;
-        $results[$key]["secret"] = $obj->photos->photo[$key]->secret;
-        $key++;
+            $results[$key]["farm"] = $obj->photos->photo[$key]->farm;
+            $results[$key]["server"] = $obj->photos->photo[$key]->server;
+            $results[$key]["id"] = $obj->photos->photo[$key]->id;
+            $results[$key]["secret"] = $obj->photos->photo[$key]->secret;
+            $key++;
+        }
+    }else {
+        echo "Aucune correspondance.";
     }
 }
 
