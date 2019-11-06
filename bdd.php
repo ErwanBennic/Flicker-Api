74aec4c276bb30b725df25a9670171eb9068628e
Emixham <Maximed.ipod@gmail.com>
2019-11-06 16:02:32 +0100

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -2,7 +2,10 @@
 
 require 'vendor/autoload.php';
 $client = new MongoDB\Client("mongodb://localhost:27017");
-$collection = $client->flickr->photos;
+$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+$db = new MongoDB\Database($manager,"flickr");
+$collection = new MongoDB\Collection($manager,$db,"photos");
+//$collection = $client->flickr->photos;
 
 $result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
 
