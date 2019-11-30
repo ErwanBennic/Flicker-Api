497802e2d74751785c5292ad3339cef62353845e
Erwan Bennic <erwanb04@gmail.com>
2019-11-30 10:51:09 +0100

    1.0

--- useApi.php
+++ useApi.php
@@ -8,13 +8,14 @@
 <?php
 require 'vendor/autoload.php';
 ini_set("allow_url_fopen", 1);
-$results = $farm = $server = $id = $secret = $total = []; /* Instanciation de plusieurs tableaux */
+$results = $total = $images = []; /* Instanciation de plusieurs tableaux */
 $url = $keyword = $min_upload = $max_upload = $title = "";
 $key = $safe_search = 0;
 $media = "all";
 $in_gallery = "false";
 
 if (isset($_POST['keyword'])) {
+    $keyword = $_POST['keyword'];
     if (isset($_POST['galery'])) {
         $in_gallery = $_POST['galery'];
     }
@@ -37,97 +38,89 @@ if (isset($_POST['keyword'])) {
         $media = "all";
     }
 
-    $keyword = $_POST['keyword'];
-
+    // Initialisation du client
     $client = new MongoDB\Client("mongodb://localhost:27017");
     $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
 
-    //$dbExist = $manager.getMongo().getDBNames().indexOf("flickr");
-    //if ($dbExist == -1){
-        //$db = new MongoDB\Database($manager,"flickr");
-    //}else{
-        $db = $client->flickr;
-    //}
+    /* Echappement des espaces pour éviter les erreurs php lors de la requête  */
+    $keywordTrm = str_replace(' ', '%20', $keyword);
+    $in_galleryTrm = str_replace(' ', '%20', $in_gallery);
 
+    /* Requête */
+    $json = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=03fcd9f31e9f53c06465ae89f019061e&text='.$keywordTrm.'&min_upload_date='.$min_upload.'&max_upload_date='.$max_upload.'&safe_search='.$safe_search.'&media='.$media.'&in_gallery='.$in_galleryTrm.'&format=json&nojsoncallback=1&extras=url_o';
+    $contents = file_get_contents($json);
+    $contents = utf8_encode($contents);
+    $obj = json_decode($contents, true);
 
+    // Vérifie si la recherche comporte des résultats -> évite de créer une base/collection inutilement
+    if ($obj['photos']['total'] != 0){
+        $isSearchEmpty = false;
+    } else {
+        $isSearchEmpty = true;
+    }
+
+    // Vérifie si la base existe, sinon la créée
+    foreach ($client->listDatabases() as $databaseInfo) {
+        if ($databaseInfo['name'] == "flickr"){
+            $db = $client->flickr;
+        } else {
+            $db = new MongoDB\Database($manager,"flickr");
+        }
+    }
+
+    // Récupération des données de la base
     $collections = $db->listCollections();
     $collectionNames = [];
     foreach ($collections as $collection) {
         $collectionNames[] = $collection->getName();
     }
+
     $exists = in_array($keyword, $collectionNames);
     if ($exists) {
         $collection = $db->$keyword;
         $collection = $collection->drop();
         $collection =  $db->createCollection($keyword);
         $collection = $db->$keyword;
-    }else {
+    }else if (!$isSearchEmpty) {
         $collection =  $db->createCollection($keyword);
         $collection = $db->$keyword;
     }
-    /* Html temporaire */
-    echo "<h1>Recherche pour \"$keyword\".</h1>";
 
-    /* Echappement des espaces pour éviter les erreurs php lors de la requête  */
-    $keywordTrm = str_replace(' ', '%20', $keyword);
-    $in_galleryTrm = str_replace(' ', '%20', $in_gallery);
-
-    /* Url */
-    $json = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=03fcd9f31e9f53c06465ae89f019061e&text='.$keywordTrm.'&min_upload_date='.$min_upload.'&max_upload_date='.$max_upload.'&safe_search='.$safe_search.'&media='.$media.'&in_gallery='.$in_galleryTrm.'&format=json&nojsoncallback=1&extras=url_o';
-    $contents = file_get_contents($json);
-    $contents = utf8_encode($contents);
-    $obj = json_decode($contents, true);
-
-    /* Vérification si le champ est bien rempli et si c'est un objet */
-
-    //if (isset($obj->photos->photo[$key]) || is_object($obj->photos->photo[$key])) {
-
-
-
+    /* Vérification et insertion dans la base */
+    if (!$isSearchEmpty) {
          foreach ($obj["photos"]["photo"] as $item) {
-             //var_dump($item);
-            $farmInt = $item['farm'];
-            $serverStr = $item['server'];
-            $idStr = $item['id'];
-            $secretStr = $item['secret'];
+            $id = $item['id'];
+            $owner = $item['owner'];
+            $farm = $item['farm'];
+            $server = $item['server'];
+            $secret = $item['secret'];
             $title = $item['title'];
+
             /* Concaténation du lien */
-            $url = "https://farm$farmInt.staticflickr.com/$serverStr/{$idStr}_{$secretStr}.jpg";
+            $url = "https://farm$farm.staticflickr.com/$server/{$id}_{$secret}.jpg";
 
             /* insertion dans la base de donnée */
              $insertOneResult = $collection->insertOne( [ 'url' => $url, 'title'=> $title, 'infos'=> $item ] );
+
             /* Affichage des images */
-             //echo "<img class='image-size' src='".$url."' />";
+            $image = "<a href='https://www.flickr.com/photos/".$owner."/".$id."'><img class='image-size' src='".$url."' /></a>";
+            array_push($images, $image);
            }
-            $results = $collection->find();
-            $TRes = iterator_to_array($results, true);
-
-            var_dump($TRes.'storage'['infos']);
-//            foreach ($TRes['infos'] as $photo ) {
-//                //var_dump($photo);
-//                //$photo = utf8_encode($photo);
-//                $c = json_decode($photo);
-//                var_dump($photo);
-//                foreach ($photo as $c) {
-//                    var_dump($c);
-//                    echo "<a href='detail.html'><img src='https://www.flickr.com/photos/".$c['owner']."/".$c['id']." /></a>";
-//                }
-//            }
-
-
-
-
-            //var_dump($farmInt);
-            //var_dump($results);
-
-//    }else {
-//        echo "Aucune correspondance.";
-//    }
-
-
+    }else {
+       echo "Aucune correspondance.";
+    }
 }
-
-        /* Affichage des images */
-        //echo "<img class='image-size' src='".$url."' />";
-
 ?>
+
+<div class="container">
+    <div class="row">
+        <h1>Recherche pour "<?php echo "$keyword" ?>"</h1>
+    </div>
+    <div class="row">
+        <?php
+        foreach ($images as $i) {
+            echo $i;
+        }
+        ?>
+    </div>
+</div>
